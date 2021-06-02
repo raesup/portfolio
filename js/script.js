7f9017674519816550e043adcbc513a9d06f47a1
unknown <raesupkim@gmail.com>
2021-06-02 16:31:23 -0600

    portfolio f

--- js/script.js
+++ js/script.js
@@ -75,32 +75,22 @@ $(document).ready(function() {
     var slideIndex = 1;
     showSlides(slideIndex);
 
-    // Next/previous controls
-
     window.plusSlides = plusSlides;
     function plusSlides(n) {
     showSlides(slideIndex += n);
     }
 
-    // Thumbnail image controls
-    // function currentSlide(n) {
-    // showSlides(slideIndex = n);
-    // }
-
     function showSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("slideImage");
-    // var dots = document.getElementsByClassName("dot");
+    var slides = document.getElementsByClassName("works");
+
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    // for (i = 0; i < dots.length; i++) {
-    //     dots[i].className = dots[i].className.replace(" active", "");
-    // }
+
     slides[slideIndex-1].style.display = "block";
-    // dots[slideIndex-1].className += " active";
     }
 
 });
