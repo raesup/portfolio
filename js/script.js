665a2962b5e3795ad0b5aaa3f1dea662e861a64e
unknown <raesupkim@gmail.com>
2021-06-01 22:50:32 -0600

    portfolio slidesshow

--- js/script.js
+++ js/script.js
@@ -72,4 +72,35 @@ $(document).ready(function() {
         $("html, body").animate({ scrollTop: targetPosition - 50}, "slow");
     });
 
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    // Next/previous controls
+
+    window.plusSlides = plusSlides;
+    function plusSlides(n) {
+    showSlides(slideIndex += n);
+    }
+
+    // Thumbnail image controls
+    // function currentSlide(n) {
+    // showSlides(slideIndex = n);
+    // }
+
+    function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slideImage");
+    // var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    // for (i = 0; i < dots.length; i++) {
+    //     dots[i].className = dots[i].className.replace(" active", "");
+    // }
+    slides[slideIndex-1].style.display = "block";
+    // dots[slideIndex-1].className += " active";
+    }
+
 });
